ea38cd9f54212053ef9e127e62dd1bca42f8ba83
Miguel Prada <mapra99@gmail.com>
2019-10-29 16:57:03 -0500

    created method to generate token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  before_create :create_remember_token
   has_secure_password
+
+  private
+  
+  def create_remember_token
+    token = SecureRandom.urlsafe_base64
+    token_digest = Digest::SHA1.hexdigest token
+    self.remember_token = token_digest
+  end
 end
