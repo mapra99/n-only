72d9207160fd7efd7a0ddbef6403958698286442
Miguel Prada <mapra99@gmail.com>
2019-10-30 13:54:52 -0500

    created posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/index'
   get 'signin', to: 'sessions#new'
   post 'signin', to: 'sessions#create'
   delete 'signin', to: 'sessions#delete'
