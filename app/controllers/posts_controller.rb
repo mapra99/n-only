1dd375e0add2d2a25817bb169f9ab4a38d64f1c0
Miguel Prada <mapra99@gmail.com>
2019-10-30 14:24:15 -0500

    added posts view, and worked the create and index actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,35 @@
+# frozen_string_literal: true
+
 class PostsController < ApplicationController
   include ApplicationHelper
 
-  before_action :users_restricted_access, only: [:new, :create]
+  before_action :users_restricted_access, only: %i[new create]
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = @current_user.id
+    if @post.save
+      render html: 'Post Saved'
+    else
+      render html: 'There was an error'
+    end
   end
 
   def index
+    @posts = Post.all
   end
 
   private
 
   def users_restricted_access
     redirect_to signin_path unless current_user
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
