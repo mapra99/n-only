daa283ee6d4e68c0ba17664ebb06446876871585
Miguel Prada <mapra99@gmail.com>
2019-10-30 13:45:27 -0500

    migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_213214) do
+ActiveRecord::Schema.define(version: 2019_10_30_184213) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 2019_10_29_213214) do
     t.string "remember_token"
   end
 
+  add_foreign_key "posts", "users"
 end
